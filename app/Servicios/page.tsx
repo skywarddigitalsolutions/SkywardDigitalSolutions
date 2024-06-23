50c3785226a72c3510e7772a5145afaa472b8dbd
Matias Ezequiel Roel <roel@fktech.net>
2024-06-23 00:50:13 -0300

    Correccion en servicios

--- app/Servicios/page.tsx
+++ app/Servicios/page.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+"use client";
 import ServicesCards from "@/components/Cards/ServicesCards";
 import TitleSections from "@/components/Sections/TitleSections";
 
 export default function Servicios() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center pt-20" style={{ backgroundImage: `url('fondo2.png')`, backgroundSize: 'cover', backgroundPosition: 'center' }} id="Servicios">
-      <TitleSections title={"Servicios"}/>
+    <div
+      className="min-h-screen flex flex-col justify-center items-center pt-20"
+      style={{
+        backgroundImage: `url('fondo2.png')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      id="Servicios"
+    >
+      <TitleSections title={"Servicios"} />
       <ServicesCards />
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,39 @@
-"use client"
-import NavBarContainer from "@/components/Navbar/NavBarContainer"
+"use client";
+import NavBarContainer from "@/components/Navbar/NavBarContainer";
 import Hero from "./Hero/page";
 import Testimonios from "./Testimonios/page";
 import Contacto from "./Contacto/page";
-import Footer from "./Footer/page"
 import Servicios from "./Servicios/page";
 import Proyectos from "./Proyectos/page";
-import Faq from "./Faq/page";
-import {Contact} from "../components/ui/contact-btn"
+import { Contact } from "../components/ui/contact-btn";
 import { useEffect, useState } from "react";
 import Loader from "@/components/ui/loader";
-import "./globals.css"
+import "./globals.css";
 export default function Home() {
-	
-	  const [loaded, setLoaded] = useState(false);
-	
-	  useEffect(() => {
-		// Simular un tiempo de carga, aquí puedes realizar cualquier lógica necesaria para cargar tus componentes.
-		setTimeout(() => {
-		  setLoaded(true); // Marcar como cargado después de 2 segundos (simulando tiempo de carga)
-		}, 700);
-	  }, []);
-	
-	return (
-	<>	{!loaded && <Loader />} {/* Muestra el Loader si no se han cargado los componentes */}
-	{loaded && (
-	  <div className="">
-		<NavBarContainer />
-		<Hero />
-		<Servicios />
-		<Proyectos />
-		<Testimonios />
-		<Contacto />
-		
-		<Contact />
-		</div>
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 700);
+  }, []);
+
+  return (
+    <>
+      {" "}
+      {!loaded && <Loader />}
+      {loaded && (
+        <div className="">
+          <NavBarContainer />
+          <Hero />
+          <Servicios />
+          <Proyectos />
+          <Testimonios />
+          <Contacto />
+
+          <Contact />
+        </div>
       )}
-	</>
-	);
+    </>
+  );
 }
