9f9bcf7f4768280bc8e6d9ce87a5b72393154b5c
agustin <agusquiroga648@gmail.com>
2024-08-02 19:08:38 -0300

    cambios preguntas

--- app/Faq/page.tsx
+++ app/Faq/page.tsx
@@ -8,39 +8,43 @@ const Faq = () => {
     {
       title: "¿Cuánto sale crear mi página web?",
       content:
-        "Tenemos distintos precios dependiendo el diseño y los tipos servicios que quieras.Tenemos atención personalizada donde te vamos a contar más sobre los servicios y vamos a asesorarte para elegir el plan que se amolde a vos",
+        "El precio varía dependiendo del diseño y los tipos servicios que quieras. Brindamos atención personalizada donde vamos a contarte más sobre los servicios y asesorarte para elegir el plan que se amolde a vos",
     },
     {
       title: "¿Ofrecen mantenimiento?",
-      content: "Si, podes agregar el servicio de mantenimiento a tu plan.",
+      content: "Si, podes agregar el servicio de mantenimiento a tu sitio web.",
     },
     {
       title: "¿Qué información necesitarán para comenzar el proyecto?",
       content:
-        "Para empezar a trabajar vamos a realizar una reunión virtual donde vamos a hacerte preguntas sobre tu marca. También ofrecemos copywriting para que no tengas que pensar los textos",
+        "Para empezar a trabajar vamos a realizar una reunión virtual donde vamos a hacerte preguntas sobre tu marca.",
     },
     {
-      title: "¿Cómo puedo realizar pagos y cuál es su política de pagos?",
+      title: "¿Cómo se realiza el pago?",
       content:
-        "En la reunión virtual se habla sobre el pago, recibimos el 50% cuando se entrega el diseño y el restante cuando se entrega el sitio web terminado",
+        "En la reunión virtual discutiremos el pago. Se abona el 50% al entregar el diseño y el 50% restante al finalizar el sitio web.",
     },
     {
       title: "¿Incluye dominio personalizado?",
-      content: "No, el valor del dominio es a parte. Podemos ayudarte a que compres el dominio",
+      content: "No, el valor del dominio es a parte. Te asesoramos con la compra de tu dominio",
     },
     {
-      title: "¿Realizan el diseño de la página o únicamente el código?",
-      content: "Ofrecemos diseño web y el desarrollo del código",
+      title: "¿Incluye hosting?",
+      content: "No, el valor del hosting es a parte. Te asesoramos con la compra de tu hosting",
+    },
+    {
+      title: "¿Realizan el diseño de maquetación antes de desarrollar el sitio?",
+      content: "Sí, proporcionamos mockups personalizados antes de comenzar con el desarrollo del sitio. Esto asegura que el diseño cumpla con sus expectativas y necesidades.",
     },
     {
       title: "¿Cuanto va a tardar mi página?",
       content:
-        "El tiempo varia dependiendo el plan y los servicios que elijas, por lo general son de 15 a 30 días",
+        "El tiempo varia dependiendo el sitio y los servicios que elijas, por lo general son de 20 a 30 días",
     },
     {
       title: "¿Qué pasa si no me gusta como queda?",
       content:
-        "Inicialmente te presentamos el diseño del sitio para que lo confirmes, luego empezamos con el código y te vamos mostrando los avances. También tenes cambios disponibles dependiendo de tu plan ",
+        "Inicialmente te presentamos el diseño del sitio para que lo confirmes, luego empezamos con el desarrollo y se coordinan reuniones para mostrar avances",
     },
     {
       title: "¿Como se manejan los cambios y revisiones durante el proceso?",
@@ -53,13 +57,9 @@ const Faq = () => {
         "Escribinos por cualquiera de nuestras redes para asesorarte y agendar una reunión",
     },
     {
-      title: "¿Qué contenido debo incluir?",
-      content: "Lo que vos quieras mostrarle a la gente. Nosotros te asesoramos para mejorar tu sitio web",
-    },
-    {
       title: "¿Cuantas secciones puede tener mi sitio web?",
       content:
-        "La cantidad que vos quieras.",
+        "La cantidad que desees",
     },
     {
       title: "¿Ofrecen el servicio de SEO?",
